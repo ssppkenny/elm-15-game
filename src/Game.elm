f52069075c1a6ca5d8e650e6486d69cde958e340
Sergey Mikhno (Galenica) <sergey.mikhno@galenica.com>
2022-06-21 09:48:54 +0200

    shuffle added

--- src/Game.elm
+++ src/Game.elm
@@ -6,6 +6,11 @@ import Html.Attributes exposing (attribute, class, classList, id, name, src, tit
 import Html.Events exposing (onClick)
 import List exposing (length)
 import List.Extra exposing (getAt)
+import List.Split exposing (chunksOfLeft)
+import Random
+import Random.List exposing (shuffle)
+import Task
+import Time exposing (..)
 
 
 mystring n =
@@ -16,10 +21,32 @@ mystring n =
         String.fromInt n
 
 
+shuffleBoard board seed =
+    let
+        flat =
+            List.concat board
+
+        s =
+            Random.initialSeed seed
+
+        g =
+            shuffle flat
+
+        shuffledList =
+            Tuple.first (Random.step g s)
+
+        newBoard =
+            chunksOfLeft 4 shuffledList
+    in
+    newBoard
+
+
 view model =
     div []
         [ div [ class "title" ]
-            [ h2 [] [ text "15 Game" ] ]
+            [ h2 [] [ text "15 Game" ]
+            , button [ class "button", onClick Shuffle ] [ text "Shuffle" ]
+            ]
         , div
             [ class "game" ]
             (List.indexedMap
@@ -34,41 +61,46 @@ view model =
                                       else
                                         class "square"
                                     , attribute "data" (String.fromInt (i * 4 + j))
-                                    , onClick { description = "Clicked", x = j, y = i }
+                                    , onClick (State { description = "Clicked", x = j, y = i })
                                     ]
                                     [ text (mystring n) ]
                             )
                             d
                         )
                 )
-                model
+                model.board
             )
         ]
 
 
 type alias Model =
-    List (List Int)
+    { time : Int, board : List (List Int) }
 
 
 neighbors x y =
     List.filter (\( a, b ) -> a >= 0 && a < 4 && b >= 0 && b < 4) [ ( x - 1, y ), ( x + 1, y ), ( x, y - 1 ), ( x, y + 1 ) ]
 
 
-getValue i j mod =
-    Maybe.withDefault -1 (getAt i (Maybe.withDefault [] (getAt j mod)))
+getValue i j board =
+    Maybe.withDefault -1 (getAt i (Maybe.withDefault [] (getAt j board)))
 
 
-type alias Msg =
-    { x : Int, y : Int, description : String }
+type Msg
+    = Tick Time.Posix
+    | Shuffle
+    | State { x : Int, y : Int, description : String }
 
 
 initialModel : Model
 initialModel =
-    [ [ 1, 2, 3, 4 ]
-    , [ 5, 6, 7, 8 ]
-    , [ 9, 10, 11, 12 ]
-    , [ 13, 14, 15, 0 ]
-    ]
+    { time = 0
+    , board =
+        [ [ 1, 2, 3, 4 ]
+        , [ 5, 6, 7, 8 ]
+        , [ 9, 10, 11, 12 ]
+        , [ 13, 14, 15, 0 ]
+        ]
+    }
 
 
 findZero msg model =
@@ -77,7 +109,7 @@ findZero msg model =
             neighbors msg.x msg.y
 
         outlist =
-            List.filter (\( i, j ) -> getValue i j model == 0) coords
+            List.filter (\( i, j ) -> getValue i j model.board == 0) coords
     in
     if length outlist == 0 then
         Nothing
@@ -87,37 +119,48 @@ findZero msg model =
 
 
 update msg model =
-    let
-        zeroCoords =
-            findZero msg model
-    in
-    case zeroCoords of
-        Just ( x, y ) ->
-            ( List.indexedMap
-                (\j row ->
-                    List.indexedMap
-                        (\i cell ->
-                            if i == x && j == y then
-                                getValue msg.x msg.y model
+    case msg of
+        Shuffle ->
+            ( { model | board = shuffleBoard model.board model.time }, Cmd.none )
 
-                            else if i == msg.x && j == msg.y then
-                                0
+        Tick newTime ->
+            ( { model | time = posixToMillis newTime }, Cmd.none )
 
-                            else
-                                cell
-                        )
-                        row
-                )
-                model
-            , Cmd.none
-            )
+        State state ->
+            let
+                zeroCoords =
+                    findZero state model
+            in
+            case zeroCoords of
+                Just ( x, y ) ->
+                    ( { model
+                        | board =
+                            List.indexedMap
+                                (\j row ->
+                                    List.indexedMap
+                                        (\i cell ->
+                                            if i == x && j == y then
+                                                getValue state.x state.y model.board
 
-        Nothing ->
-            ( model, Cmd.none )
+                                            else if i == state.x && j == state.y then
+                                                0
+
+                                            else
+                                                cell
+                                        )
+                                        row
+                                )
+                                model.board
+                      }
+                    , Cmd.none
+                    )
+
+                Nothing ->
+                    ( model, Cmd.none )
 
 
 initialCmd =
-    Cmd.none
+    Task.perform Tick Time.now
 
 
 main : Program () Model Msg
